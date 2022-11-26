de7ff231ea4e8d33e98c2d8c5ea7898d63d7c4f9
DeveloperMoha <moha201565@yahoo.com>
2022-11-26 04:01:07 +0200

    day1 ui challenge

---
+++ lib/core/utils/constants.dart
@@ -0,0 +1,9 @@
+class Day1Constants {
+  static const promoImage1 = "assets/images/day1_images/one.jpg";
+  static const promoImage2 = "assets/images/day1_images/two.jpg";
+  static const promoImage3 = "assets/images/day1_images/three.jpg";
+  static const promoImage4 = "assets/images/day1_images/four.jpg";
+  static const promoImage5 = "assets/images/day1_images/five.jpg";
+  static const promoImage6 = "assets/images/day1_images/six.jpg";
+  static const boxCover = "assets/images/day1_images/cover.jpg";
+}
